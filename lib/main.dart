8f5847b0768e61a72ca88501a28c7cbec90d0058
Horace Ho <horaceh@gmail.com>
2020-02-20 14:53:20 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Office',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomePage(title: 'Office'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '...',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
